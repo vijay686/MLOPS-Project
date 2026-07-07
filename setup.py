7042d8733d1cce3b09897a03c8847f4363afe573
Vijay Kumar <vijaykumarkandagatla55@gmail.com>
2026-07-07 22:52:02 +0530

    Added Template with required to be filled.

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="src",
+    version="0.0.1",
+    author="Vijay Kumar",
+    author_email="vijaykumarkandagatla55@gmail.com",
+    packages=find_packages()
+)
